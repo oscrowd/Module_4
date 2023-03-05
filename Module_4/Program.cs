7bccd225a19f6645466b07a78383ca62013a0102
oscrowd <oscrowd@gmail.com>
2023-03-05 21:13:58 +0300

    Программа-анкета о себе

--- Module_4/Program.cs
+++ Module_4/Program.cs
@@ -10,9 +10,15 @@ namespace Module_4
     {
         static void Main(string[] args)
         {
-            String MyName;
-            MyName = "Roman";
-            Console.WriteLine(MyName);
+            const String MyName = "Roman";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
             Console.ReadKey();
         }
     }
