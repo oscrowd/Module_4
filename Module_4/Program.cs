dc33c735a26a42380b461662bf54a8850c997beb
oscrowd <oscrowd@gmail.com>
2023-03-05 15:52:56 +0300

    Добавьте файлы проекта.

---
+++ Module_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String MyName;
+            MyName = "Roman";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
